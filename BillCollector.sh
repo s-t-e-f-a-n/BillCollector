1540fc17b264e80471a2326c609581f74370d0fd
s-t-e-f-a-n <s-c-h-m-i-t-t@web.de>
2025-04-15 23:20:43 +0200

    Improved error checks in bitwarden_api_check_status Introduced commit_id file to track commit IDs

--- BillCollector.sh
+++ BillCollector.sh
@@ -6,5 +6,13 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
+if [[ -f .commit_id ]]; then
+    COMMIT_ID=$(cat .commit_id)
+    echo "Commit-ID: $COMMIT_ID"
+else
+    echo "Error: .commit_id not found. Check .git/hooks/pre-commit exists with following content:
+    echo 'git rev-parse HEAD > .commit_id'"
+fi
+
 docker run -v $SCRIPT_DIR/apps/Downloads:/apps/Downloads --rm billcollector:latest \
        python3 ./BillCollector.py $1 $2
